2d236d39a556b0a4a0ce41112f8bfecb7326c043
Kyeoungwoon Park <65695112+kyeoungwoon@users.noreply.github.com>
2022-03-03 10:55:00 +0900

    .

--- BOJ/Python/test.py
+++ BOJ/Python/test.py
@@ -1,4 +1,10 @@
-import numpy as np
-
-b = np.array([1], [2])
-print(b)
+n = int(input())
+a, b = 0, 1
+if n==0:
+    print(0)
+elif n==1:
+    print(1)
+else:
+    for x in range (n-1):
+        a, b = b, a+b
+    print(b)
--- programmers/위클리 챌린지/3주차/test.py
+++ programmers/위클리 챌린지/3주차/test.py
@@ -1,4 +1,15 @@
 import os
 
-url = 'https://www.youtube.com/watch?v=ndizAUAR_Cs'
-os.system(f"youtube-dl {url} -f 251")
+urls = [
+    'https://youtube.com/playlist?list=PLONB62YjBmH3gbK5ENl8QErUBdGzuN9ws',
+    'https://youtube.com/playlist?list=PLucdnvOsr3ss4vimHlpSY9PHVXOQIFSmM',
+    'https://www.youtube.com/watch?v=zo8ZxIp88SM&list=PLucdnvOsr3svZb1QwIz039cu9ySOUVhn1&index=2',
+    'https://www.youtube.com/watch?v=39bVN8A4Ia0&list=PLucdnvOsr3svZb1QwIz039cu9ySOUVhn1&index=3',
+    'https://www.youtube.com/watch?v=YcTwsmqsnZ8&list=PLucdnvOsr3svZb1QwIz039cu9ySOUVhn1&index=4',
+    'https://www.youtube.com/watch?v=5kGF8wyNt2Q&list=PLucdnvOsr3sud8PsjyhLQVyrkrUpjJz69&index=1',
+    'https://www.youtube.com/watch?v=fsys7_MutCo&list=PLucdnvOsr3sud8PsjyhLQVyrkrUpjJz69&index=2'
+    
+]
+
+for url in urls:
+    os.system(f"youtube-dl {url} -x --audio-format best --audio-quality 0 -i")
